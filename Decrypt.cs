35fc4a41f19b763a4e6b6b44be01e16b6df7d128
JPage4 <jesse.page@me.com>
2023-06-09 12:10:06 -0500

    initial commit

---
+++ Decrypt.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarDecryption
+{
+    public class Decrypt
+    {
+        public string DecryptMessage(string message, int rotationKey)
+        {
+            string[] words = message.Split("_");
+            List<string> decryptedWords = new List<string>();
+
+            for (int i = 0; i < words.Length - 1; i += 2)
+            {
+                // Remove spaces from the encrypted word
+                string encryptedWord = words[i].Replace(" ", "");
+
+                //look at following number, if number > word.length, trim off last character
+                int followingNumberWordLength = int.Parse(words[i + 1]);
+                string correctedWord = encryptedWord.Substring(0, Math.Min(encryptedWord.Length, followingNumberWordLength));
+
+                //Reverses the KEY shift value to each char in word
+                decryptedWords.Add(CaesarDecrypt(correctedWord, rotationKey));
+            }
+
+            return string.Join(" ", decryptedWords).Trim();
+        }
+
+        private string CaesarDecrypt(string message, int rotation)
+        {
+            //I learned about the Caesar Cipher! I'd never heard of it before this
+            var decryptedChars = new List<char>();
+            foreach (char c in message)
+            {
+                int rotatedChar = c - rotation;
+                if (c >= 65 && c <= 90)//if uppercase
+                {
+                    if (rotatedChar >= 65)
+                        decryptedChars.Add((char)rotatedChar);
+                    else//if rotatedChar is out of bounds, go to end of uppercase letters
+                        decryptedChars.Add((char)(91 - (65 - rotatedChar)));
+                }
+                else if (c >= 97 && c <= 122)//if lowercase
+                {
+                    if (rotatedChar >= 97)
+                        decryptedChars.Add((char)rotatedChar);
+                    else //if rotatedChar is out of bounds, go to end of lowercase letters
+                        decryptedChars.Add((char)(123 - (97 - rotatedChar)));
+                }
+                else
+                {//if not a letter, just add to list
+                    decryptedChars.Add(c);
+                }
+            }
+
+            return string.Join("", decryptedChars);
+        }
+
+        public int GetShiftValue(string message)
+        {
+            Dictionary<char, int> frequencyAnalysis = CalculateFrequencyAnalysis(message.ToUpper());
+            char mostFrequentLetter = MostFrequentLetter(frequencyAnalysis);
+            int shiftValue = CalculateShiftValue(mostFrequentLetter, 'E');
+            return shiftValue;
+        }
+
+        private Dictionary<char, int> CalculateFrequencyAnalysis(string text)
+        {
+            Dictionary<char, int> frequencyAnalysis = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {//counts occurance for each letter
+                if (char.IsLetter(c))
+                {
+                    if (frequencyAnalysis.ContainsKey(c))
+                        frequencyAnalysis[c]++;
+                    else
+                        frequencyAnalysis[c] = 1;
+                }
+            }
+
+            return frequencyAnalysis;
+        }
+
+        private char MostFrequentLetter(Dictionary<char, int> frequencyAnalysis)
+        {// returns most frequent by the highest Key count
+            KeyValuePair<char, int> mostFrequent = frequencyAnalysis.OrderByDescending(x => x.Value).FirstOrDefault();
+            return mostFrequent.Key;
+        }
+
+        private int CalculateShiftValue(char encryptedLetter, char decryptedLetter)
+        {
+            int encryptedAscii = (int)encryptedLetter;
+            int decryptedAscii = (int)decryptedLetter;
+
+            // Calculate the shift value
+            int shiftValue = encryptedAscii - decryptedAscii;
+
+            return shiftValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace CaesarDecryption
+{
+    public class Program
+    {
+        const string SECRET_MESSAGE = "Ez ol jd _5_ jz f7 _3_ sl gp _4_ nc ln vp o9 _7_ l6 _1_ df ap cQ _5_ dp nc pe _6_ nz op _4_ es le _4_ hl dz _3_ py nc ja ep oH _9_ fd ty ru _5_ ly _2_ lw rz ct es xC _9_ op gt dp oy _7_ mj _2_ l9 _1_ ep lx _4_ zq _2_ dp nf ct ej _8_ da pn tl wt de dZ _11_ ez _2_ mp _2_ tx az dd tm wp _10_ ez _2_ nc ln v. _6_ Gp cj _4_ hp ww _4_ oz yp !M _5_";
+
+        public static void Main()
+        {
+            // THIS IS JUST AN EXAMPLE FOR ILLUSTRATIVE PURPOSES. 
+            // IT IS NOT RELATED TO THE SECRET MESSAGE.
+
+            string exampleMessage = "Just a test.";
+            int exampleKey = 4;
+
+            string exampleResult = EncryptMessage(exampleMessage, exampleKey);
+            Console.WriteLine("Encrypted example: " + exampleResult);
+
+            Decrypt decrypt = new Decrypt();
+            //Originally I ran a for loop 26 to find what the shift key should be
+            //a better way to do this is to search SECRET_MESSAGE for the letter that occurs most frequently and
+            //calculate the shift based off of "E" which is the letter that statistically occurs most often
+
+            int shiftValue = decrypt.GetShiftValue(SECRET_MESSAGE);
+            string decryptResult = decrypt.DecryptMessage(SECRET_MESSAGE, shiftValue);
+            Console.WriteLine("Decrypted example: " + decryptResult);
+
+        }
+
+        //Below is starting code given by Applied Systems to solve decryption code challenge
+        //SECRET MESSAGE WAS CREATED USING THIS METHOD.
+        public static string EncryptMessage(string message, int key)
+        {
+            var encryptedMessage = CaesarEncrypt(message, key);
+            return FormatMessage(encryptedMessage, 2);
+        }
+
+        private static string CaesarEncrypt(string message, int rotation)
+        {
+            // ASCII letter ranges
+            // A - 65, Z - 90
+            // a - 97, z - 122
+
+            var rotatedChars = new List<char>();
+
+            foreach (char c in message)
+            {
+                int rotatedChar = c + rotation;
+                if (c >= 65 && c <= 90)
+                {
+                    if (rotatedChar <= 90)
+                        rotatedChars.Add((char)rotatedChar);
+                    else
+                        rotatedChars.Add((char)(64 + (rotatedChar - 90)));
+                }
+                else if (c >= 97 && c <= 122)
+                {
+                    if (rotatedChar <= 122)
+                        rotatedChars.Add((char)rotatedChar);
+                    else
+                        rotatedChars.Add((char)(96 + (rotatedChar - 122)));
+                }
+                else
+                {
+                    rotatedChars.Add(c);
+                }
+            }
+
+            return string.Join("", rotatedChars);
+        }
+
+        private static string FormatMessage(string message, int padding)
+        {
+            var words = message.Split();
+            var preparedWords = new List<string>();
+            foreach (var word in words)
+            {
+                preparedWords.Add(SplitAndPadWord(word, padding));
+            }
+            return string.Join(" ", preparedWords).Trim();
+        }
+
+        private static string SplitAndPadWord(string word, int padding)
+        {
+            int numberOfParts = (int)Math.Ceiling((double)word.Length / padding);
+
+            string result;
+            if (numberOfParts > 1)
+            {
+                List<string> wordParts = new List<string>();
+
+                for (int part = 0; part < numberOfParts; part++)
+                {
+                    if (part < (numberOfParts - 1))
+                        wordParts.Add(word.Substring(part * padding, padding));
+                    else
+                    {
+                        var unpaddedPart = word.Substring(part * padding);
+                        wordParts.Add(PadPart(unpaddedPart, padding));
+                    }
+                }
+                result = string.Join(" ", wordParts.ToArray()).Trim();
+            }
+            else
+                result = PadPart(word, padding);
+
+            return string.Format("{0} _{1}_", result, word.Length);
+        }
+
+        private static string PadPart(string part, int padding)
+        {
+            var sb = new StringBuilder(part);
+            var paddingNeeded = padding - part.Length;
+
+            for (int iteration = 0; iteration < paddingNeeded; iteration++)
+                sb.Append(GetRandomChar());
+
+            return sb.ToString();
+        }
+
+        private static Random rand = new Random();
+        private static char GetRandomChar()
+        {
+            var possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return possibleChars[rand.Next(0, possibleChars.Length)];
+        }
+    }
+}
